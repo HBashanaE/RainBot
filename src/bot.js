6ce6600415a33d87a0bd10f628b705fb6de43db0
Bashana Elikewela <hbashanae@gmail.com>
2022-12-23 09:40:15 +0530

    initial commit

---
+++ src/bot.js
@@ -0,0 +1,20 @@
+const { Bot } = require("grammy");
+
+require('dotenv').config()
+
+// Create a bot object
+const bot = new Bot(process.env.TOKEN);
+
+// Register listeners to handle messages
+bot.on("message:text", (ctx) => {
+    fetch(`https://goweather.herokuapp.com/weather/${ctx.message.text}`)
+    .then((response) => response.json())
+    .then((body) => {
+        console.log(body);
+        ctx.reply(`Temperature: ${body.temperature}\nWind: ${body.wind}\nOverall state: ${body.description}`);
+    }); 
+  
+});
+
+// Start the bot (using long polling)
+bot.start();
